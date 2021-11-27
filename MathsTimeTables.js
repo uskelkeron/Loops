f525c8abf7d80b6de948e2e5dc34ec134435d4a8
Admin <admin@Admins-MacBook-Pro.local>
2021-11-27 12:47:54 -0400

    Continuously prompt user and exit clause

--- MathsTimeTables.js
+++ MathsTimeTables.js
@@ -1,9 +1,30 @@
 const prompt = require("prompt-sync")();
+let escape = true;
 
-const number = parseInt(prompt("please enter which tables you would like to lean: "));
+do{
 
-for(let i=1; i<=12; i++)
-{
-    console.log(` ${i} x ${number} = ${number*i}`);
+    const number = parseInt(prompt("please enter which tables you would like to lean: "));
+
+
+    for(let i=1; i<=12; i++)
+    {
+        if (number <= 1)
+        {
+            escape = false;
+            break;
+        }
+
+        else 
+        {
+            console.log(` ${i} x ${number} = ${number*i}`);
+        }
+       
+    }
+
+    
 }
+
+while(escape == true)
+
+
                                                                                                                                                                                                                                                                                                                          
--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## Question 1: Simple Loop saying I love you
 
-## Question 2: Generate Maths Tables 
+## Question 2: Generate Maths Tables ** MathsTimeTable.js
+
+## Question 2: Modifed to to allowed user to enter the Maths table they want
+
+## Question 2: Modifed to continueouly prompt user to enter the tables they want.
